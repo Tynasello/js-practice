a68f9cd2a5ee5ff6d677edfec267069010bee78c
Tynasello <tynasello@gmail.com>
2021-07-10 15:15:40 -0400

    still in progress

--- to-do-app/README.md
+++ to-do-app/README.md
@@ -4,32 +4,6 @@ import all modules to index.js
 
 ---
 
-NAV
-
-LEFT SECTION
-Sections => to do, today, this week section, and projects drop-down
-projects drop-down shows all projects created and has a new project button at top
-Needs to be hamburger style menu
-
-RIGHT SECTION
-lists all tasks in current task grouping
-has a title and then an unordered list
-
-TO-DO TASK
-each activity has a title, project category and description. There's due date with a priority option.
-List format => first thing, checkbox for completion. Then task name. On the right side there is an edit option, a change priority option, and delete.
-edit and priority options open small popups
-Edit option => header with title and exit button. then section with all task attributes. bottom has close and update task buttons
-Onclick => when any task is clicked, a dropdown appears below with all attributes.
-
-ADDING AND EDITING TASKS
-When confrirm add or confirm edit buttons are pressed, get the user input in each field. If date is either today or this week, put it in the appropriate
-subfolder. Also check if the project subfolder that the user inputted exists, either put it in there or create that folder and then add it.
-make sure user input is not case sensitive.
-
-Onclick of the calender, a pop-up appears
-On click of completion button, the text color changes to grey and text appears crossed out.
-
 ---
 
 DRAG AND DROP FUNCTIONALITY OF LIST ITEMS
--- to-do-app/src/index.js
+++ to-do-app/src/index.js
@@ -1,7 +1,5 @@
 const projectsBtn = document.getElementById("projects-title");
 const projectsList = document.getElementById("current-projects-list");
-const addProjectBtn = document.getElementById("add-project-btn");
-const addTaskBtn = document.getElementById("add-task-button");
 
 projectsBtn.addEventListener("click", projectsDropdown);
 
@@ -17,6 +15,8 @@ function projectsDropdown() {
 
 ---------------- */
 
+//PROJECT DESC DROPDOWN ----------------------
+
 // const task = document.querySelector(".task");
 // task.addEventListener("click", taskClicked);
 
@@ -28,35 +28,204 @@ function projectsDropdown() {
 //   }
 // }
 
-// const taskCompletedLabel = document.getElementById("sometask");
-// taskCompletedLabel.addEventListener("click", taskCompleted);
-// function taskCompleted() {
-//   if (
-//     this.parentElement.querySelector("label").style["text-decoration"] ==
-//     "line-through"
-//   ) {
-//     this.parentElement.querySelector("label").style["text-decoration"] = "none";
-//     this.parentElement.parentElement.parentElement.style.color = "#566573";
-//   } else {
-//     this.parentElement.querySelector("label").style["text-decoration"] =
-//       "line-through";
-//     this.parentElement.parentElement.parentElement.style.color = "#B3B6B7";
-//   }
+// EDIT TASK --------------------------------
+
+// const editBtn = document.querySelector(".task-edit-btn");
+// editBtn.addEventListener("click", editTask);
+
+// function editTask() {
+//   overlay.classList.add("active");
+//   modalContainer.classList.add("active");
+//   modalTitle.textContent = "Edit Task";
+//   taskConfirmBtn.textContent = "Confirm Edit"
 // }
 
-const editBtn = document.querySelector(".task-edit-btn");
-editBtn.addEventListener("click", editTask);
-const modalContainer = document.querySelector(".modal-container");
-const overlay = document.getElementById("overlay");
-const exitEditTaskBtn = document.querySelector(".close-modal");
-overlay.addEventListener("click", removeOverlay);
-exitEditTaskBtn.addEventListener("click", removeOverlay);
+//
 
-function editTask() {
-  overlay.classList.add("active");
-  modalContainer.classList.add("active");
+// ----------------------------------------------------------------
+const overlay = document.getElementById("overlay");
+
+let subfolders = JSON.parse(localStorage.getItem("subfolders")) || [];
+let selectedSubfolder = localStorage.getItem("selectedSubfolder");
+/*----- 
+HELPER FUNCTIONS
+-----*/
+function removeFromDOM(element) {
+  element.textContent = "";
 }
+
+/*----- 
+UPDATING DOM / LOCAL STORAGE 
+-----*/
+const projectListContainer = document.getElementById("current-projects-list");
+const activeProjectHeader = document.getElementById("tasks-of-subfolder");
+const tasksContainer = document.getElementById("tasks-list");
+
+function updateDOMStorage() {
+  updateDOM();
+  localStorage.setItem("subfolders", JSON.stringify(subfolders));
+  localStorage.setItem("selectedSubfolder", selectedSubfolder);
+}
+
+function updateDOM() {
+  removeFromDOM(projectListContainer);
+  displaySubfolders();
+  activeProjectHeader.textContent = `${selectedSubfolder.name}`;
+  removeFromDOM(tasksContainer);
+  displayTasks(selectedSubfolder);
+}
+
+function displaySubfolders() {
+  subfolders.forEach(subfolder, () => {
+    const li = document.createElement("li");
+    li.classList.add("project");
+    li.id = subfolder.id;
+    if (li.id == selectedSubfolder.id) {
+      li.classList.add("active-subfolder");
+    }
+    const h3 = document.createElement("h3");
+    h3.textContent = `→ ${subfolder.name}`;
+    li.appendChild(h3);
+    projectListContainer.append(li);
+  });
+}
+
+function displayTasks(subfolder) {
+  subfolder.forEach(task, () => {
+    const li = document.createElement("li");
+    li.classList.add("task");
+    li.innerHTML = `
+    <div class="task-above">
+      <div class="task-left">
+        <input
+          type="checkbox"
+          id="${task.id}"
+          onclick="event.stopPropagation()"
+          ${task.complete}
+        />
+        <label> ${task.name}</label><br />
+      </div>
+      <div class="task-right">
+        <i
+          class="fas fa-edit task-edit-btn btn"
+          onclick="event.stopPropagation()"
+        ></i>
+        <i
+          class="fas fa-flag priority-flag"
+          onclick="event.stopPropagation()"
+        ></i>
+        <i
+          class="fas fa-trash-alt task-delete-btn btn"
+          onclick="event.stopPropagation()"
+        ></i>
+      </div>
+    </div>
+    <div class="task-info-dropdown">
+      <div class="task-info-dropdown-left">
+        <h3>Name: ${task.name}</h3>
+        <h3>Description: ${task.description}</h3>
+      </div>
+      <div class="task-info-dropdown-right">
+        <h3>Project: ${task.projectUnder}</h3>
+        <h3>Due Date: ${task.dueDate}</h3>
+        <h3>Priority Level: ${task.priority}</h3>
+      </div>
+    </div>`;
+    let completedTaskBtn = li.querySelector(`input#${task.id}`);
+    let deleteTaskBtn = li.querySelector("i.task-delete-btn");
+    addTaskFunctionality(task, completedTaskBtn, deleteTaskBtn);
+  });
+}
+/*----- 
+TASK FUNCTIONALITY
+-----*/
+
+function addTaskFunctionality(task, completedTaskBtn, deleteTaskBtn) {
+  completedTaskBtn.addEventListener("click", taskCompleted(task));
+  deleteTaskBtn.addEventListener("click", removeTask(task));
+}
+function taskCompleted(task) {
+  const taskLabel = task.querySelector("label");
+  if (taskLabel.style["text-decoration"] == "line-through") {
+    taskLabel.style["text-decoration"] = "none";
+    task.style.color = "#566573";
+  } else {
+    taskLabel.style["text-decoration"] = "line-through";
+    task.style.color = "#B3B6B7";
+  }
+}
+function removeTask(task) {
+  tasksContainer.removeChild(task);
+}
+/*----- 
+PROJECT / SUBFOLDER --- CREATION / UPDATE
+-----*/
+const subfolderModal = document.getElementById("subfolder-modal");
+const addProjectBtn = document.getElementById("add-subfolder-btn");
+const submitProjectBtn = document.getElementById("confirm-subfolder");
+addProjectBtn.addEventListener("click", projectPopUp);
+function projectPopUp() {
+  overlay.classList.add("active");
+  subfolderModal.classList.add("active");
+}
+submitProjectBtn.addEventListener("click", () => {
+  const newSubfolderName = subfolderModal.querySelector(
+    "input#subfolder-title-name-input"
+  ).value;
+  if (newSubfolderName == "" || newSubfolderName == null) return;
+  newSubfolder = {
+    id: Date.now().toString(),
+    name: newSubfolderName,
+    tasks: [],
+  };
+  // subfolders.push(newSubfolder);
+  removeOverlay();
+  // updateDomStorage();
+});
+
+/*----- 
+TASK --- CREATION / UPDATE
+-----*/
+const submitTaskBtn = document.getElementById("confirm-task");
+const taskModal = document.getElementById("task-modal");
+const addTaskBtn = document.getElementById("add-task-button");
+const modalTitle = document.querySelector("h3.modal-title");
+
+addTaskBtn.addEventListener("click", taskPopUp);
+function taskPopUp() {
+  overlay.classList.add("active");
+  taskModal.classList.add("active");
+  modalTitle.textContent = "Add Task";
+  submitTaskBtn.querySelector(".btn").textContent = "Add Task";
+}
+
+submitTaskBtn.addEventListener("click", () => {
+  const newTask = {
+    id: Date.now().toString(),
+    name: taskModal.querySelector("input#task-title-name-input").value,
+    description: taskModal.querySelector("textarea#description").value,
+    projectUnder: taskModal.querySelector("input#task-project-input").value,
+    dueDate: taskModal.querySelector("input#task-due-date-input").value,
+    priority: taskModal.querySelector("select#task-priority-input").value,
+    complete: "",
+  };
+  let selectedSubfolder = localStorage.getItem("selectedSubfolder");
+  // selectedSubfolder.tasks.push(newTask);
+  removeOverlay();
+  // updateDOMStorage();
+});
+
+/*----- 
+REMOVE OVERLAY
+-----*/
+const removeOverlayBtns = document.querySelectorAll(".close-modal");
+
+removeOverlayBtns.forEach((btn) => {
+  btn.addEventListener("click", removeOverlay);
+});
+
 function removeOverlay() {
   overlay.classList.remove("active");
-  modalContainer.classList.remove("active");
+  taskModal.classList.remove("active");
+  subfolderModal.classList.remove("active");
 }
